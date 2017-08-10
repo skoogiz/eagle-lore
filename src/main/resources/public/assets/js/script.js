2dc0e7de7a7798a2bfd903dcf00dba8cd75c11c8
skoogiz <skoogiz@gmail.com>
2017-08-11 01:12:09 +0200

    Added snackbar

--- src/main/resources/public/assets/js/script.js
+++ src/main/resources/public/assets/js/script.js
@@ -6,9 +6,15 @@ $(document).ready(() => {
         $("#abilities").append(createAbility());
     });
 
+    $("#create-monster").click((event) => {
+        event.preventDefault();
+        showSnackbar("Nytt monster skapat");
+    });
+
     $("#generate-monster").click((event) => {
         event.preventDefault();
         $("#result-container").html(createMonsterPanel($("#race-selector").val()));
+        showSnackbar("Nytt monster genererat");
     });
 });
 
@@ -37,4 +43,43 @@ const createMonsterPanel = (text) => {
     $(p).text(text);
     $(panel).append(p);
     return panel;
+};
+
+const addSnackbar = (parent) => {
+    var snackbar = $(document.createElement("div"));
+    $(snackbar).addClass("snackbar");
+    var text = $(document.createElement("div"));
+    $(text).addClass("snackbar-text");
+    $(snackbar).append(text);
+    $(parent).append(snackbar);
+};
+
+const showSnackbar = (text) => {
+    if (!($("#snackbar-root").length)) return;
+    var root = $("#snackbar-root");
+    if (!($(".snackbar").length)) addSnackbar(root);
+    $(root).removeClass("snackbar-root-hidden");
+    $(root).addClass("snackbar-root-visible");
+    showSnackbarText(text);
+    setTimeout(() => {
+        hideSnackbarText();
+        $(root).removeClass("snackbar-root-visible");
+        $(root).addClass("snackbar-root-hidden");
+    }, 3000);
+};
+
+const hideSnackbarText = () => {
+    var snack = $(".snackbar-text");
+    $(snack).removeClass("snackbar-text-visible");
+    $(snack).addClass("snackbar-text-hidden");
+    $(snack).html("");
+};
+
+const showSnackbarText = (text) => {
+    var snack = $(".snackbar-text");
+    $(snack).removeClass("snackbar-text-hidden");
+    $(snack).addClass("snackbar-text-visible");
+    var span = $(document.createElement("span"));
+    $(span).text(text);
+    $(snack).html(span);
 };
