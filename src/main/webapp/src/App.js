bd73d120531a2ecbc2777ef8aaf9b723b376e7d1
skoogiz <skoogiz@gmail.com>
2017-12-06 20:37:53 +0100

    played played around with routes pt 1

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar';
 import logo from './logo.svg';
 import './App.css';
@@ -17,11 +17,13 @@ const PrimaryLayout = () => (
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/users">Users</Link></li>
+      <li><Link to="/monsters">Monsters</Link></li>
       <li><Link to="/about">About</Link></li>
     </ul>
     <main className="App-intro">
       <Route path="/" exact component={HomePage} />
       <Route path="/users" component={UsersPage} />
+      <Route path="/monsters" component={MonsterSubLayout} />
       <Route path="/about" component={AboutPage} />
     </main>
   </div>
@@ -30,6 +32,30 @@ const PrimaryLayout = () => (
 const HomePage =() => <div>Home Page</div>;
 const UsersPage = () => <div>Users Page</div>;
 const AboutPage = () => <div>To get started, edit <code>src/App.js</code> and save to reload.</div>;
+
+const BrowseMonstersPage = () => <div>Browse Monsters Page</div>;
+const CreateMonsterPage = () => <div>Create Monster Page</div>;
+
+const UserNav = ({ match }) => (
+  <nav className="context-nav">
+    <NavLink to={`${match.path}`} exact activeClassName="active">Browse</NavLink>
+    <NavLink to={`${match.path}/create`} activeClassName="active">Create</NavLink>
+  </nav>
+);
+
+const MonsterSubLayout = ({ match }) => (
+  <div className="user-sub-layout">
+    <aside>
+      <UserNav />
+    </aside>
+    <div className="primary-content">
+      <Switch>
+        <Route path={`${match.path}`} exact component={BrowseMonstersPage} />
+        <Route path={`${match.path}/create`} component={CreateMonsterPage} />
+      </Switch>
+    </div>
+  </div>
+)
 
 const App = () => (
   <BrowserRouter>
