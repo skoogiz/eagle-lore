e274e33fe8c870b15cf28469b5bb030a75dc1542
skoogiz <skoogiz@gmail.com>
2017-12-13 18:24:21 +0100

    exprementing with the appbar

--- src/main/webapp/src/layouts/PrimaryLayout.js
+++ src/main/webapp/src/layouts/PrimaryLayout.js
@@ -1,19 +1,28 @@
 import React from "react";
-import { Route, Link, Switch, Redirect } from "react-router-dom";
+import { Route, Link, Switch, Redirect, withRouter } from "react-router-dom";
 import AppBar from "material-ui/AppBar";
 import AboutPage from "../pages/AboutPage";
 import HomePage from "../pages/HomePage";
 import MonsterSubLayout from "./MonsterSubLayout";
 
+const NavBar = withRouter(({ history }) => (
+  <AppBar
+    title="Eagle Lore"
+    onTitleClick={() => { history.push("/"); }}
+    showMenuIconButton={false}
+  >
+    <ul>
+      <li><Link to="/monsters">Monsters</Link></li>
+      <li><Link to="/about">About</Link></li>
+    </ul>
+  </AppBar>
+));
+
 const PrimaryLayout = () => (
   <div>
-    <AppBar
-      title="Eagle Lore"
-      iconClassNameRight="muidocs-icon-navigation-expand-more"
-    />
+    <NavBar />
     <header>
       <ul>
-        <li><Link to="/">Home</Link></li>
         <li><Link to="/monsters">Monsters</Link></li>
         <li><Link to="/about">About</Link></li>
       </ul>
