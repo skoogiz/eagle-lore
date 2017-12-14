42c8bf40b806386f78a1b5db6436199643bbd1f6
skoogiz <skoogiz@gmail.com>
2017-12-15 00:15:10 +0100

    appbar

--- src/main/webapp/src/layouts/PrimaryLayout.js
+++ src/main/webapp/src/layouts/PrimaryLayout.js
@@ -4,29 +4,24 @@ import AppBar from "material-ui/AppBar";
 import AboutPage from "../pages/AboutPage";
 import HomePage from "../pages/HomePage";
 import MonsterSubLayout from "./MonsterSubLayout";
+import "./PrimaryLayout.css";
 
 const NavBar = withRouter(({ history }) => (
   <AppBar
-    title="Eagle Lore"
+    title={<span className="title">Eagle Lore</span>}
     onTitleClick={() => { history.push("/"); }}
     showMenuIconButton={false}
   >
-    <ul>
-      <li><Link to="/monsters">Monsters</Link></li>
-      <li><Link to="/about">About</Link></li>
-    </ul>
+    <nav className="navbar">
+      <Link to="/monsters">Monsters</Link>
+      <Link to="/about">About</Link>
+    </nav>
   </AppBar>
 ));
 
 const PrimaryLayout = () => (
   <div>
     <NavBar />
-    <header>
-      <ul>
-        <li><Link to="/monsters">Monsters</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-    </header>
     <main>
       <Switch>
         <Route path="/" exact component={HomePage} />
